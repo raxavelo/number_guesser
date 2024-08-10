57f8178442e2cf17478dfd92c142a2bee0867e29
raxavelo <raxavelo@gmail.com>
2024-08-10 18:54:41 +0200

    Added main logic

--- number_guesser.py
+++ number_guesser.py
@@ -10,3 +10,21 @@ if top_of_range.isdigit():
 else:
     print("Next time, please enter a number.")
     exit()
+
+random_number = random.randint(0, top_of_range)
+
+while True:
+    user_guess = input("Guess a number between 0 and " + str(top_of_range))
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please, next time enter a number.")
+        continue
+
+    if user_guess == random_number:
+        print("Congratulations! You guessed the correct number.")
+        break
+    elif user_guess < random_number:
+        print("Too low. Try again.")
+    else:
+        print("Too high. Try again.")
